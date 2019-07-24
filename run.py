0c263a90046f912ada7a7b845cd67d86ff32951b
Yuke Liang <coconutlyk@gmail.com>
2019-07-23 23:17:04 -0700

    Added parameter

--- run.py
+++ run.py
@@ -308,9 +308,9 @@ def estimate(tensorFirst, tensorSecond):
 
 ##########################################################
 
-def estimate_wrapper():
-	tensorFirst = torch.FloatTensor(numpy.array(PIL.Image.open(arguments_strFirst))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
-	tensorSecond = torch.FloatTensor(numpy.array(PIL.Image.open(arguments_strSecond))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+def estimate_wrapper(First, Second):
+	tensorFirst = torch.FloatTensor(numpy.array(PIL.Image.open(First))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+	tensorSecond = torch.FloatTensor(numpy.array(PIL.Image.open(Second))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
 	tensorOutput = estimate(tensorFirst, tensorSecond)
 	return numpy.array(tensorOutput.numpy().transpose(1, 2, 0), numpy.float32)
 
